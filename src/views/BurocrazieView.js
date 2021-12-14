62b8baf49fbf54fb2fff3ef52be5940a50bae937
Bruni Michele <michelebruni87@gmail.com>
2021-12-14 23:07:57 +0100

    modificato stile burocrazia

--- src/views/BurocrazieView.js
+++ src/views/BurocrazieView.js
@@ -6,21 +6,21 @@ import './BurocrazieView.css';
 
 function BurocrazieView(props) {
   
-    return (<div className="re-box re-box-burocrazie"> 
-                <div className="row row-cols-5 testata-burocrazie">
-                  <div className="col"><FontAwesomeIcon icon={faCalendar} /></div>
-                  <div className="col"><FontAwesomeIcon icon={faWallet} /></div>
-                  <div className="col"><FontAwesomeIcon icon={faIndustry} /></div>
-                  <div className="col"><FontAwesomeIcon icon={faPiggyBank} /></div>
-                  <div className="col"><FontAwesomeIcon icon={faHandHoldingUsd} /></div>
+    return (<div className="re-box re-box-burocrazie px-3"> 
+                <div className="row row-cols-4 testata-burocrazie">
+                  <div className="col anno"><FontAwesomeIcon icon={faCalendar} /></div>
+                  <div className="col iniz"><FontAwesomeIcon icon={faWallet} /></div>
+                  <div className="col cost"><FontAwesomeIcon icon={faIndustry} /></div>
+                  {/* <div className="col"><FontAwesomeIcon icon={faPiggyBank} /></div> */}
+                  <div className="col entr"><FontAwesomeIcon icon={faHandHoldingUsd} /></div>
                 </div>
                 {props.burocrazie.map((obj, i) => 
-                <div key={i} className="row row-cols-5">
-                  <div className="col">{obj.Anno}</div>
-                  <div className="col">{obj.ValoreIniziale}</div>
-                  <div className="col">{obj.Costi}</div>
-                  <div className="col">{obj.ValoreIniziale !== undefined && obj.Costi !== undefined ? obj.ValoreIniziale - obj.Costi : ''}</div>
-                  <div className="col">{obj.Guadagni}</div>
+                <div key={i} className="row row-cols-4">
+                  <div className="col anno">{obj.Anno}</div>
+                  <div className="col iniz">{obj.ValoreIniziale}</div>
+                  <div className="col cost">{obj.Costi !== undefined ? "- " + obj.Costi : ""}</div>
+                  {/* <div className="col">{obj.ValoreIniziale !== undefined && obj.Costi !== undefined ? obj.ValoreIniziale - obj.Costi : ''}</div> */}
+                  <div className="col entr">{obj.Guadagni !== undefined ? (obj.Guadagni > 0 ? "+ " + obj.Guadagni : "- "  + obj.Guadagni*-1) : ""}</div>
                 </div>)}
             </div>);
   }
